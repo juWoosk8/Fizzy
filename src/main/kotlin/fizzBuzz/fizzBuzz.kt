487ff08c2b9e8c9446c8217dc659e6bf55531349
julie.woodford <julie.woodford@johnlewis.co.uk>
2020-02-28 11:33:18 +0000

    add further tests

--- src/main/kotlin/fizzBuzz/fizzBuzz.kt
+++ src/main/kotlin/fizzBuzz/fizzBuzz.kt
@@ -1,8 +1,12 @@
 package fizzBuzz
 
-fun fizzbuzz(input: String): String{
-    return input
+fun fizzbuzz(input: Int): String {
+    if (input % 15 == 0) return "FizzBuzz"
+    if (input % 3 == 0) return "Fizz"
+    if (input % 5 == 0) return "Buzz"
+    return input.toString()
 }
+
 fun main() {
 
 }
--- src/test/kotlin/fizzBuzzTest/fizzyTest.kt
+++ src/test/kotlin/fizzBuzzTest/fizzyTest.kt
@@ -10,21 +10,49 @@ internal class FizzBuzzTest {
     @Test
 
     fun `should return 1 given 1 `() {
-        val result = fizzbuzz("1")
+        val result = fizzbuzz(1)
         assertThat(result).isEqualTo("1")
     }
 
     @Test
 
     fun `should return 2 given 2 `() {
-        val result = fizzbuzz("2")
+        val result = fizzbuzz(2)
         assertThat(result).isEqualTo("2")
     }
 
     @Test
 
     fun `should return Fizz given 3 `() {
-        val result = fizzbuzz("3")
+        val result = fizzbuzz(3)
         assertThat(result).isEqualTo("Fizz")
+    }
+
+    @Test
+
+    fun `should return Buzz given 5 `() {
+        val result = fizzbuzz(5)
+        assertThat(result).isEqualTo("Buzz")
+    }
+
+    @Test
+
+    fun `should return Fizz given 6 `() {
+        val result = fizzbuzz(6)
+        assertThat(result).isEqualTo("Fizz")
+    }
+
+    @Test
+
+    fun `should return Buzz given 10 `() {
+        val result = fizzbuzz(10)
+        assertThat(result).isEqualTo("Buzz")
+    }
+
+    @Test
+
+    fun `should return FizzBuzz given 15 `() {
+        val result = fizzbuzz(15)
+        assertThat(result).isEqualTo("FizzBuzz")
     }
 }
