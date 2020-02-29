c7493faf29089444d729d364bfd62ad125cc62de
julie.woodford <julie.woodford@johnlewis.co.uk>
2020-02-29 20:04:47 +0000

    add code for vertical moves

--- src/main/kotlin/jlpCodingChallenges/challenge3.kt
+++ src/main/kotlin/jlpCodingChallenges/challenge3.kt
@@ -1,56 +1,64 @@
 package jlpCodingChallenges
 
 fun processSokobanMove(squareArray: Array<String>, move: String): Array<String> {
+    val row = squareArray.size
+    val column = squareArray[0].length
+    var nestedArray = Array(row) { CharArray(column) }
+    for (i in 0 until row) {
+        for (j in 0 until column) {
+            nestedArray[i][j] = squareArray[i].toCharArray()[j]
+        }
+    }
     when (move) {
-        "L" -> moveHorizontal(squareArray, -1)
-        "R" -> moveHorizontal(squareArray, 1)
+        "L" -> move(nestedArray, -1, 0, column)
+        "R" -> move(nestedArray, 1, 0, column)
+        "D" -> move(nestedArray, 0, 1, column)
+        "U" -> move(nestedArray, 0, -1, column)
+    }
+    for (i in 0..squareArray.lastIndex) {
+        squareArray[i] = nestedArray[i].joinToString("")
     }
     return squareArray
 }
 
-fun moveHorizontal(squareArray: Array<String>, dir :Int): Array<String> {
-    for (i in 0..squareArray.lastIndex) {
-        if (squareArray[i].contains("p")) {
-            var lineCharArray = squareArray[i].toCharArray()
-            loop@ for (j in 0..lineCharArray.lastIndex) {
-                if (lineCharArray[j] == 'p') {
-                    when (lineCharArray[j + dir]) {
-
-                        '#' -> break@loop
-                        ' ' -> {
-                            lineCharArray[j + dir] = 'p'
-                            lineCharArray[j] = ' '
-                            break@loop
-                        }
-                        '*' -> {
-                            lineCharArray[j + dir] = 'P'
-                            lineCharArray[j] = ' '
-                            break@loop
-                        }
-                        'b' -> {
-                            when (lineCharArray[j + dir*2]) {
-                                '#', 'b' -> break@loop
-                                ' ' -> {
-                                    lineCharArray[j + dir*2] = 'b'
-                                    lineCharArray[j + dir] = 'p'
-                                    lineCharArray[j] = ' '
-                                    break@loop
-                                }
-                                '*' -> {
-                                    lineCharArray[j + dir*2] = 'B'
-                                    lineCharArray[j + dir] = 'p'
-                                    lineCharArray[j] = ' '
-                                    break@loop
-                                }
+fun move(nestedArray: Array<CharArray>, dirHor: Int, dirVert: Int, column: Int): Array<CharArray> {
+    for (i in 0..nestedArray.lastIndex) {
+        loop@ for (j in 0 until column) {
+            if (nestedArray[i][j].toUpperCase() == 'P') {
+                when (nestedArray[i + dirVert][j + dirHor]) {
+                    '#' -> break@loop
+                    ' ' -> {
+                        nestedArray[i + dirVert][j + dirHor] = 'p'
+                        nestedArray[i][j] = ' '
+                        break@loop
+                    }
+                    '*' -> {
+                        nestedArray[i + dirVert][j + dirHor] = 'P'
+                        nestedArray[i][j] = ' '
+                        break@loop
+                    }
+                    'b' -> {
+                        when (nestedArray[i + dirVert * 2][j + dirHor * 2]) {
+                            '#', 'b' -> break@loop
+                            ' ' -> {
+                                nestedArray[i + dirVert * 2][j + dirHor * 2] = 'b'
+                                nestedArray[i + dirVert][j + dirHor] = 'p'
+                                nestedArray[i][j] = ' '
+                                break@loop
+                            }
+                            '*' -> {
+                                nestedArray[i + dirVert * 2][j + dirHor * 2] = 'B'
+                                nestedArray[i + dirVert][j + dirHor] = 'p'
+                                nestedArray[i][j] = ' '
+                                break@loop
                             }
                         }
                     }
                 }
             }
-            squareArray[i] = lineCharArray.joinToString("")
         }
     }
-    return squareArray
+    return nestedArray
 }
 
 
