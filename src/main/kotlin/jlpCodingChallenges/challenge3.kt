190255b5748c87b7c510c19dc9adf35a38e721b5
julie.woodford <julie.woodford@johnlewis.co.uk>
2020-02-29 20:20:28 +0000

    tidying up

--- src/main/kotlin/jlpCodingChallenges/challenge3.kt
+++ src/main/kotlin/jlpCodingChallenges/challenge3.kt
@@ -3,17 +3,17 @@ package jlpCodingChallenges
 fun processSokobanMove(squareArray: Array<String>, move: String): Array<String> {
     val row = squareArray.size
     val column = squareArray[0].length
-    var nestedArray = Array(row) { CharArray(column) }
+    val nestedArray = Array(row) { CharArray(column) }
     for (i in 0 until row) {
         for (j in 0 until column) {
             nestedArray[i][j] = squareArray[i].toCharArray()[j]
         }
     }
     when (move) {
-        "L" -> move(nestedArray, -1, 0, column)
-        "R" -> move(nestedArray, 1, 0, column)
-        "D" -> move(nestedArray, 0, 1, column)
-        "U" -> move(nestedArray, 0, -1, column)
+        "L" -> move(nestedArray, -1, 0)
+        "R" -> move(nestedArray, 1, 0)
+        "D" -> move(nestedArray, 0, 1)
+        "U" -> move(nestedArray, 0, -1)
     }
     for (i in 0..squareArray.lastIndex) {
         squareArray[i] = nestedArray[i].joinToString("")
@@ -21,34 +21,34 @@ fun processSokobanMove(squareArray: Array<String>, move: String): Array<String> 
     return squareArray
 }
 
-fun move(nestedArray: Array<CharArray>, dirHor: Int, dirVert: Int, column: Int): Array<CharArray> {
+fun move(nestedArray: Array<CharArray>, x: Int, y: Int): Array<CharArray> {
     for (i in 0..nestedArray.lastIndex) {
-        loop@ for (j in 0 until column) {
+        loop@ for (j in nestedArray[0].indices) {
             if (nestedArray[i][j].toUpperCase() == 'P') {
-                when (nestedArray[i + dirVert][j + dirHor]) {
+                when (nestedArray[i + y][j + x]) {
                     '#' -> break@loop
                     ' ' -> {
-                        nestedArray[i + dirVert][j + dirHor] = 'p'
+                        nestedArray[i + y][j + x] = 'p'
                         nestedArray[i][j] = ' '
                         break@loop
                     }
                     '*' -> {
-                        nestedArray[i + dirVert][j + dirHor] = 'P'
+                        nestedArray[i + y][j + x] = 'P'
                         nestedArray[i][j] = ' '
                         break@loop
                     }
                     'b' -> {
-                        when (nestedArray[i + dirVert * 2][j + dirHor * 2]) {
+                        when (nestedArray[i + y * 2][j + x * 2]) {
                             '#', 'b' -> break@loop
                             ' ' -> {
-                                nestedArray[i + dirVert * 2][j + dirHor * 2] = 'b'
-                                nestedArray[i + dirVert][j + dirHor] = 'p'
+                                nestedArray[i + y * 2][j + x * 2] = 'b'
+                                nestedArray[i + y][j + x] = 'p'
                                 nestedArray[i][j] = ' '
                                 break@loop
                             }
                             '*' -> {
-                                nestedArray[i + dirVert * 2][j + dirHor * 2] = 'B'
-                                nestedArray[i + dirVert][j + dirHor] = 'p'
+                                nestedArray[i + y * 2][j + x * 2] = 'B'
+                                nestedArray[i + y][j + x] = 'p'
                                 nestedArray[i][j] = ' '
                                 break@loop
                             }
@@ -59,9 +59,4 @@ fun move(nestedArray: Array<CharArray>, dirHor: Int, dirVert: Int, column: Int):
         }
     }
     return nestedArray
-}
-
-
-fun main() {
-
 }
